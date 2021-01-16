daed044f4cc27f612a70d6a18078256269c9f458
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2021-01-16 10:54:05 -0700

    Lesson 2 work

---
+++ lesson2/js/script.js
@@ -0,0 +1,9 @@
+const today = new Date();
+
+const date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
+
+const time = today.getHours() + ':' + today.getMinutes();
+
+const currentDateTime = date + ' ' + time;
+
+document.getElementById('date').innerHTML = currentDateTime;
